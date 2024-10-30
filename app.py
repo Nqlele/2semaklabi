0c492c37cb9c40aa2208d761b49e6b14a75db86f
Еремин Захар <Aneqyal@yandex.ru>
2024-10-30 15:23:10 +0700

    oshibki

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ app.register_blueprint(lab1)
 @app.route("/index")
 def start():
     return redirect("/menu", code=302)
+@app.errorhandler(404)
+def not_found(err):
+    return "нет такой страницы", 404
+@app.errorhandler(500)
+def server_error(err):
+    return "ошибка сервера", 500
 
 @app.route("/menu")
 def menu():
