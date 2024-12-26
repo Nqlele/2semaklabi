b931e26f7443a21502f5bd1320603686ff62d3b7
Еремин Захар <Aneqyal@yandex.ru>
2024-12-26 17:34:36 +0700

    podkl baza

--- lab5.py
+++ lab5.py
@@ -1,19 +1,28 @@
-from flask import Blueprint, render_template, request, redirect, session
+from flask import Blueprint, render_template, request, redirect, session, current_app
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
+import sqlite3
+from os import path
 lab5 = Blueprint('lab5', __name__)
 @lab5.route('/lab5/')
 def lab():
     return render_template('lab5/lab5.html', login=session.get('login', 'Anonymous'))
 def db_connect():
-    conn = psycopg2.connect(
-        host = '127.0.0.1',
-        database = 'alexey_stepuk_knowledge_base',
-        user = 'alexey_stepuk_knowledge_base',
-        password = '123'
-    )
-    cur = conn.cursor(cursor_factory =  RealDictCursor)
+    if current_app.config['DB_TYPE'] == 'postgres':
+        conn = psycopg2.connect(
+            host = '127.0.0.1',
+            database = 'eremin_zakhar_knowledge_base',
+            user = 'eremin_zakhar_knowledge_base',
+            password = '123'
+        )
+        cur = conn.cursor(cursor_factory =  RealDictCursor)
+    else:
+        dir_path = path.dirname(path.realpath(__file__))
+        db_path = path.join(dir_path, "database.db")
+        conn = sqlite3.connect(db_path)
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
     return conn, cur
 def db_close(conn, cur):
     conn.commit()
@@ -28,13 +37,13 @@ def register():
     if not (login and password):
         return render_template('lab5/register.html', error='Заполните все поля')
     conn, cur = db_connect()
-    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    cur.execute(f"SELECT login FROM users WHERE login=%s;", (login, ))
     if cur.fetchone():
         db_close(conn, cur)
         return render_template('lab5/register.html', error='Такой пользователь уже существует')
     
     password_hash = generate_password_hash(password)
-    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password_hash}');")
+    cur.execute("INSERT INTO users (login, password) VALUES (%s, %s);", (login, password_hash))
 
     db_close(conn, cur)
     return render_template('lab5/success.html', login=login)
@@ -78,8 +87,8 @@ def create():
     conn, cur = db_connect()
     cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
     user_id = cur.fetchone()["id"]
-    cur.execute(f"INSERT INTO articles(user_id, title, article_text) \
-                VALUES ({user_id}, '{title}', '{article_text}')")
+    cur.execute("INSERT INTO articles(user_id, title, article_text) \
+                VALUES (%s, %s, %s);", (user_id, title, article_text))
     db_close(conn, cur)
     return redirect('/lab5')
 @lab5.route('/lab5/list')
@@ -88,9 +97,9 @@ def list():
     if not login:
         return redirect('/lab5/login')
     conn, cur = db_connect()
-    cur.execute(f"SELECT id FROM users WHERE login='{login}';")
+    cur.execute("SELECT id FROM users  WHERE login=%s;", (login, ))
     user_id = cur.fetchone()["id"]
-    cur.execute(f"SELECT * FROM articles WHERE user_id='{user_id}';")
+    cur.execute("SELECT * FROM articles WHERE user_id=%s;", (user_id, ))
     articles = cur.fetchall()
     db_close(conn, cur)
     return render_template('/lab5/articles.html', articles=articles)
